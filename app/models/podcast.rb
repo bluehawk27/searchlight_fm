19bdd9bf539da70d5a19fb1f53163458d6a5bd66
bluehawk27 <avillacorta27@gmail.com>
2015-11-14 20:46:41 -0800

    deletes episode when podcast is deleted

--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -3,7 +3,7 @@ class Podcast < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :episodes
+  has_many :episodes, dependent: :destroy
   has_attached_file :thumbnail, styles: { large: "1000x1000#", medium: "150x150#" }
   validates_attachment_content_type :thumbnail, content_type: /\Aimage\/.*\Z/
 end
